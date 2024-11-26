e904a81d12e1284ec6f131254ec071e86dd98d53
Pikurrot <pikurrot@gmail.com>
2024-11-26 10:27:19 +0100

    Use transformers dataset, solve bugs and train

--- train_CL_embs.py
+++ train_CL_embs.py
@@ -1,35 +1,18 @@
 import os
 import torch
 import sqlite3
-from torch.utils.data import Dataset
-from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, SentenceTransformerTrainingArguments, losses
-
-
-class SQLiteDataset(Dataset):
-	def __init__(self, db_file):
-		self.conn = sqlite3.connect(db_file)
-		self.cursor = self.conn.cursor()
-		# Get dataset size
-		self.cursor.execute("SELECT COUNT(*) FROM pairs")
-		self.size = self.cursor.fetchone()[0]
-
-	def __len__(self):
-		return self.size
-
-	def __getitem__(self, idx):
-		self.cursor.execute("SELECT anchor, positive FROM pairs WHERE id = ?", (idx + 1,))
-		row = self.cursor.fetchone()
-		if row is None:
-			raise IndexError(f"Index {idx} out of range")
-		return {"anchor": row[0], "positive": row[1]}
-
-	def close(self):
-		self.conn.close()
+from datasets import Dataset
+from sentence_transformers import (
+    SentenceTransformer,
+    SentenceTransformerTrainer,
+    SentenceTransformerTrainingArguments,
+    losses,
+)
 
 
 def train_CL_embs(
 		model: SentenceTransformer,
-		dataset: SQLiteDataset,
+		dataset: Dataset,
 		**kwargs
 ):
 	print("Training the embeddings for CL...")
@@ -40,7 +23,9 @@ def train_CL_embs(
 	args = SentenceTransformerTrainingArguments(
 		output_dir=output_dir,
 		per_device_train_batch_size=64,
-		num_train_epochs=1,
+		num_train_epochs=10,
+		logging_steps=1,
+		report_to="wandb"
 	)
 	trainer = SentenceTransformerTrainer(
 		model=model,
@@ -53,7 +38,7 @@ def train_CL_embs(
 
 if __name__ == "__main__":
 	# Prepare model and dataset
-	os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3,4,5"
+	os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(str(i) for i in range(1,10))
 	db_file_path = "/data3fast/users/elopez/data/cl_trainset.db"
 	embed_model_name = "BAAI/bge-small-en-v1.5"
 	cache_dir = "/data3fast/users/elopez/models"
@@ -63,9 +48,13 @@ if __name__ == "__main__":
 	embed_model.to(device)
 	print("Using device:", device)
 	print("Loading trainset...")
-	dataset = SQLiteDataset(db_file_path)
+	conn = sqlite3.connect(db_file_path)
+	dataset = Dataset.from_sql(
+		sql="SELECT anchor, positive FROM trainset",
+		con=conn,
+	)
 
 	# Train the embeddings
 	train_CL_embs(embed_model, dataset, output_dir="/data3fast/users/elopez/models/bge-finetuned")
-	dataset.close()
+	conn.close()
 	print("Done!")

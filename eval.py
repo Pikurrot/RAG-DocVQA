bab15f41af55a14cf4610c1808ccaa7c408b689d
Pikurrot <pikurrot@gmail.com>
2025-01-28 10:58:34 +0100

    Enable save continuously

--- eval.py
+++ eval.py
@@ -17,6 +17,93 @@ from src.HiVT5 import Proxy_HiVT5
 from typing import Union
 
 
+def save_data(
+		config: dict,
+		filename: str,
+		eval_res: dict
+):
+	inf_time_f = eval_res["inf_time"]
+	inf_time = time_stamp_to_hhmmss(inf_time_f, string=True)
+	avg_load_time = time_stamp_to_hhmmss(eval_res["avg_load_time"], string=True)
+	avg_layout_time = time_stamp_to_hhmmss(eval_res["avg_layout_time"], string=True)
+	avg_retrieval_time = time_stamp_to_hhmmss(eval_res["avg_retrieval_time"], string=True)
+	avg_generation_time = time_stamp_to_hhmmss(eval_res["avg_generation_time"], string=True)
+	total_load_time = time_stamp_to_hhmmss(eval_res["total_load_time"], string=True)
+	total_layout_time = time_stamp_to_hhmmss(eval_res["total_layout_time"], string=True)
+	total_retrieval_time = time_stamp_to_hhmmss(eval_res["total_retrieval_time"], string=True)
+	total_generation_time = time_stamp_to_hhmmss(eval_res["total_generation_time"], string=True)
+
+	for key, stat in eval_res["retrieval_stats"].items():
+		if isinstance(stat, Counter):
+			mean = np.mean(list(stat.elements()))
+			std = np.std(list(stat.elements()))
+			min_val = min(stat.elements())
+			max_val = max(stat.elements())
+
+			# save only relevant distribution
+			stat_relevant = Counter()
+			# top 5 most common values
+			for k, v in stat.most_common(5):
+				stat_relevant[k] = v
+			# top 5 least common values
+			for k, v in stat.most_common()[:-6:-1]:
+				stat_relevant[k] = v
+			# smallest 5 keys
+			for k in sorted(stat.keys())[:5]:
+				stat_relevant[k] = stat[k]
+			# largest 5 keys
+			for k in sorted(stat.keys())[:-6:-1]:
+				stat_relevant[k] = stat[k]
+			# top 5 keys around the mean
+			for k in sorted(stat.keys(), key=lambda x: abs(x-mean))[:5]:
+				stat_relevant[k] = stat[k]
+
+			eval_res["retrieval_stats"][key] = {
+				"mean": mean,
+				"std": std,
+				"min": min_val,
+				"max": max_val,
+				"relevant_samples": dict(stat_relevant)
+			}
+	
+	save_data = {
+		"Model": config["model_name"],
+		"Model weights": config["model_weights"],
+		"Embed weights": config.get("embed_weights", "-"),
+		"Layout model weigths": config.get("layout_model_weights", "-"),
+		"Dataset": config["dataset_name"],
+		"Page retrieval": config.get("page_retrieval", "-").capitalize(),
+		"embed_model": config.get("embed_model", "-"),
+		"chunk_num": config.get("chunk_num", "-"),
+		"chunk_size": config.get("chunk_size", "-"),
+		"overlap": config.get("overlap", "-"),
+		"include_surroundings": config.get("include_surroundings", "-"),
+		"Avg accuracy": eval_res["accuracy"],
+		"Avg ANLS": eval_res["anls"],
+		"Avg retrieval precision": eval_res["retrieval_precision"],
+		"Avg chunk score": eval_res["chunk_score"],
+		"Dataset size": f"{data_size*100}%",
+		"Inference time": inf_time,
+		"Avg load time": avg_load_time,
+		"Avg retrieval time (layout)": avg_layout_time,
+		"Avg retrieval time": avg_retrieval_time,
+		"Avg generation time": avg_generation_time,
+		"Total load time": f"{total_load_time} ({eval_res['total_load_time']/inf_time_f*100:.2f}%)",
+		"Total retrieval time (layout)": f"{total_layout_time} ({eval_res['total_layout_time']/inf_time_f*100:.2f}%)",
+		"Total retrieval time": f"{total_retrieval_time} ({eval_res['total_retrieval_time']/inf_time_f*100:.2f}%)",
+		"Total generation time": f"{total_generation_time} ({eval_res['total_generation_time']/inf_time_f*100:.2f}%)",
+		"Retrieval stats": eval_res["retrieval_stats"],
+		"Scores by samples": eval_res["scores_by_samples"]
+	}
+
+	metrics_file = os.path.join(config["save_dir"], "metrics", config["save_folder"], filename)
+	results_file = os.path.join(config["save_dir"], "results", config["save_folder"], filename)
+	save_json(metrics_file, save_data)
+	print("Metrics correctly saved in: {:s}".format(metrics_file))
+	if eval_res["results"]:
+		save_json(results_file, eval_res["results"])
+		print("Results correctly saved in: {:s}".format(results_file))
+
 def evaluate(
 		data_loader: DataLoader,
 		model: Union[RAGVT5, Proxy_HiVT5],
@@ -26,6 +113,8 @@ def evaluate(
 	return_scores_by_sample = kwargs.get("return_scores_by_sample", False)
 	return_answers = kwargs.get("return_answers", False)
 	save_results = kwargs.get("save_results", False)
+	start_time = kwargs.get("start_time", time.time())
+	save_continuously = kwargs.get("save_continuously", False)
 	model_name = model.__class__.__name__
 	model_name = "Hi-VT5" if model_name == "Proxy_HiVT5" else model_name
 
@@ -56,14 +145,14 @@ def evaluate(
 
 		# Inference using the model
 		if model_name == "Hi-VT5":
-			start_time = time.time()
+			start_gen_time = time.time()
 			_, pred_answers, pred_answer_pages, pred_answers_conf = model.inference(
 				batch,
 				return_pred_answer=True
 			)
 			retrieval = {
 				"retrieval_time": 0,
-				"generation_time": time.time() - start_time
+				"generation_time": time.time() - start_gen_time
 			}
 		elif model_name == "RAGVT5":
 			_, pred_answers, _, pred_answers_conf, retrieval = model.inference(
@@ -159,36 +248,50 @@ def evaluate(
 		torch.cuda.empty_cache()
 		# print(torch.cuda.memory_summary(device=None, abbreviated=False))
 
-	if not return_scores_by_sample:
-		# Compute average metrics
-		total_accuracies = total_accuracies/len(data_loader.dataset)
-		total_anls = total_anls/len(data_loader.dataset)
-		total_ret_prec = total_ret_prec/len(data_loader.dataset)
-		total_chunk_scores = total_chunk_scores/len(data_loader.dataset)
-		scores_by_samples = []
-	avg_load_time = load_time/len(data_loader)
-	avg_layout_time = layout_time/len(data_loader)
-	avg_retrieval_time = retrieval_time/len(data_loader)
-	avg_generation_time = generation_time/len(data_loader)
+		# Save data
+		if save_continuously or (b == len(data_loader) - 1):
+			if return_scores_by_sample:
+				accuracy = np.mean(total_accuracies)
+				anls = np.mean(total_anls)
+				retrieval_precision = np.mean(total_ret_prec)
+				avg_chunk_score = np.mean(total_chunk_scores)
+			else:
+				# Compute average metrics
+				accuracy = np.mean(total_accuracies/len(data_loader.dataset))
+				anls = np.mean(total_anls/len(data_loader.dataset))
+				retrieval_precision = np.mean(total_ret_prec/len(data_loader.dataset))
+				avg_chunk_score = np.mean(total_chunk_scores/len(data_loader.dataset))
+				scores_by_samples = []
+			avg_load_time = load_time/len(data_loader)
+			avg_layout_time = layout_time/len(data_loader)
+			avg_retrieval_time = retrieval_time/len(data_loader)
+			avg_generation_time = generation_time/len(data_loader)
 
-	return {
-		"accuracy": total_accuracies,
-		"anls": total_anls,
-		"retrieval_precision": total_ret_prec,
-		"chunk_score": total_chunk_scores,
-		"all_pred_answers": all_pred_answers,
-		"scores_by_samples": scores_by_samples,
-		"avg_load_time": avg_load_time,
-		"avg_layout_time": avg_layout_time,
-		"avg_retrieval_time": avg_retrieval_time,
-		"avg_generation_time": avg_generation_time,
-		"total_load_time": load_time,
-		"total_layout_time": layout_time,
-		"total_retrieval_time": retrieval_time,
-		"total_generation_time": generation_time,
-		"retrieval_stats": retrieval_stats,
-		"results": results
-	}
+			res = {
+				"accuracy": accuracy,
+				"anls": anls,
+				"retrieval_precision": retrieval_precision,
+				"chunk_score": avg_chunk_score,
+				"all_pred_answers": all_pred_answers,
+				"scores_by_samples": scores_by_samples,
+				"avg_load_time": avg_load_time,
+				"avg_layout_time": avg_layout_time,
+				"avg_retrieval_time": avg_retrieval_time,
+				"avg_generation_time": avg_generation_time,
+				"total_load_time": load_time,
+				"total_layout_time": layout_time,
+				"total_retrieval_time": retrieval_time,
+				"total_generation_time": generation_time,
+				"retrieval_stats": retrieval_stats,
+				"results": results
+			}
+
+			# Save data
+			inf_time_f = time.time() - start_time
+			res["inf_time"] = inf_time_f
+			save_data(config, filename, res)
+
+	return res
 
 
 if __name__ == "__main__":
@@ -209,12 +312,19 @@ if __name__ == "__main__":
 		"visible_devices": "1",
 		# "model_weights": "/data3fast/users/elopez/checkpoints/ragvt5_concat_mp-docvqa_sep-token/model__9.ckpt"
 		"embed_weights": "/data3fast/users/elopez/models/bge-finetuned-2/checkpoint-820",
-		"layout_model_weights": "cmarkea/dit-base-layout-detection"
+		"layout_model_weights": "cmarkea/dit-base-layout-detection",
+		"save_folder": "8-layout_model"
 	}
 	os.environ["CUDA_VISIBLE_DEVICES"] = args["visible_devices"]
 	args = argparse.Namespace(**args)
 	config = load_config(args)
 	start_time = time.time()
+	experiment_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+	if config["model_name"] == "Hi-VT5":
+		filename = "hivt5_{:}.json".format(experiment_date)
+	else:
+		filename = "{:}_{:}_{:}.json".format(config.get("embed_model", "").lower(), config.get("page_retrieval", "").lower(), experiment_date)
+
 	print("Building model...")
 	model = build_model(config)
 	model.to(config["device"])
@@ -226,104 +336,16 @@ if __name__ == "__main__":
 	# Evaluate the model
 	print("Evaluating...")
 	evaluator = Evaluator(case_sensitive=False)
-	eval_res = evaluate(
+	evaluate(
 		val_data_loader,
 		model, evaluator,
 		return_scores_by_sample=True,
 		return_answers=True,
 		save_results=False,
-		chunk_num=config.get("chunk_num", 10),
-		chunk_size=config.get("chunk_size", 60),
-		overlap=config.get("overlap", 10),
-		include_surroundings=config.get("include_surroundings", 0)
+		save_continuously=True,
+		chunk_num=config["chunk_num"],
+		chunk_size=config["chunk_size"],
+		overlap=config["overlap"],
+		include_surroundings=config["include_surroundings"],
+		start_time=start_time
 	)
-	accuracy = np.mean(eval_res["accuracy"])
-	anls = np.mean(eval_res["anls"])
-	retrieval_precision = np.mean(eval_res["retrieval_precision"])
-	avg_chunk_score = np.mean(eval_res["chunk_score"])
-	
-	# Save results
-	inf_time_f = time.time() - start_time
-	inf_time = time_stamp_to_hhmmss(inf_time_f, string=True)
-	avg_load_time = time_stamp_to_hhmmss(eval_res["avg_load_time"], string=True)
-	avg_layout_time = time_stamp_to_hhmmss(eval_res["avg_layout_time"], string=True)
-	avg_retrieval_time = time_stamp_to_hhmmss(eval_res["avg_retrieval_time"], string=True)
-	avg_generation_time = time_stamp_to_hhmmss(eval_res["avg_generation_time"], string=True)
-	total_load_time = time_stamp_to_hhmmss(eval_res["total_load_time"], string=True)
-	total_layout_time = time_stamp_to_hhmmss(eval_res["total_layout_time"], string=True)
-	total_retrieval_time = time_stamp_to_hhmmss(eval_res["total_retrieval_time"], string=True)
-	total_generation_time = time_stamp_to_hhmmss(eval_res["total_generation_time"], string=True)
-
-	for key, stat in eval_res["retrieval_stats"].items():
-		if isinstance(stat, Counter):
-			mean = np.mean(list(stat.elements()))
-			std = np.std(list(stat.elements()))
-			min_val = min(stat.elements())
-			max_val = max(stat.elements())
-
-			# save only relevant distribution
-			stat_relevant = Counter()
-			# top 5 most common values
-			for k, v in stat.most_common(5):
-				stat_relevant[k] = v
-			# top 5 least common values
-			for k, v in stat.most_common()[:-6:-1]:
-				stat_relevant[k] = v
-			# smallest 5 keys
-			for k in sorted(stat.keys())[:5]:
-				stat_relevant[k] = stat[k]
-			# largest 5 keys
-			for k in sorted(stat.keys())[:-6:-1]:
-				stat_relevant[k] = stat[k]
-			# top 5 keys around the mean
-			for k in sorted(stat.keys(), key=lambda x: abs(x-mean))[:5]:
-				stat_relevant[k] = stat[k]
-
-			eval_res["retrieval_stats"][key] = {
-				"mean": mean,
-				"std": std,
-				"min": min_val,
-				"max": max_val,
-				"relevant_samples": dict(stat_relevant)
-			}
-
-	save_data = {
-		"Model": config["model_name"],
-		"Model_weights": config["model_weights"],
-		"Embed_weights": config["embed_weights"],
-		"Dataset": config["dataset_name"],
-		"Page retrieval": config.get("page_retrieval", "-").capitalize(),
-		"embed_model": config.get("embed_model", "-"),
-		"chunk_num": config.get("chunk_num", "-"),
-		"chunk_size": config.get("chunk_size", "-"),
-		"overlap": config.get("overlap", "-"),
-		"include_surroundings": config.get("include_surroundings", "-"),
-		"Avg accuracy": accuracy,
-		"Avg ANLS": anls,
-		"Avg retrieval precision": retrieval_precision,
-		"Avg chunk score": avg_chunk_score,
-		"Dataset size": f"{data_size*100}%",
-		"Inference time": inf_time,
-		"Avg load time": avg_load_time,
-		"Avg retrieval time (layout)": avg_layout_time,
-		"Avg retrieval time": avg_retrieval_time,
-		"Avg generation time": avg_generation_time,
-		"Total load time": f"{total_load_time} ({eval_res['total_load_time']/inf_time_f*100:.2f}%)",
-		"Total retrieval time (layout)": f"{total_layout_time} ({eval_res['total_layout_time']/inf_time_f*100:.2f}%)",
-		"Total retrieval time": f"{total_retrieval_time} ({eval_res['total_retrieval_time']/inf_time_f*100:.2f}%)",
-		"Total generation time": f"{total_generation_time} ({eval_res['total_generation_time']/inf_time_f*100:.2f}%)",
-		"Retrieval stats": eval_res["retrieval_stats"],
-		"Scores by samples": eval_res["scores_by_samples"]
-	}
-	experiment_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-	if config["model_name"] == "Hi-VT5":
-		filename = "hivt5_{:}.json".format(experiment_date)
-	else:
-		filename = "{:}_{:}_{:}.json".format(config.get("embed_model", "").lower(), config.get("page_retrieval", "").lower(), experiment_date)
-	metrics_file = os.path.join(config["save_dir"], "metrics", filename)
-	results_file = os.path.join(config["save_dir"], "results", filename)
-	save_json(metrics_file, save_data)
-	print("Metrics correctly saved in: {:s}".format(metrics_file))
-	if eval_res["results"]:
-		save_json(results_file, eval_res["results"])
-		print("Results correctly saved in: {:s}".format(results_file))

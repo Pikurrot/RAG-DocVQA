be0d259c31797edcb8b6f0c264d954dba2c3181a
Pikurrot <pikurrot@gmail.com>
2025-03-03 11:15:32 +0100

    Remove s2chunking original code

--- src/s2chunking/__init__.py
+++
@@ -1,4 +0,0 @@
-from .s2_chunker import StructuralSemanticChunker, generate_nodes, generate_edges, format_clusters
-from .layout_deter import LayoutDetector
-
-__all__ = ['StructuralSemanticChunker', 'generate_nodes', 'generate_edges', 'format_clusters', 'LayoutDetector']
